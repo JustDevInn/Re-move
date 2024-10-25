b9c729381a6852a37e1e66615db211eea5e9cdf1
Justin Peeters <peeters.justin@yahoo.com>
2024-10-25 16:04:46 +0800

    added the original text to werkwijze onderdelen

--- src/components/Werkwijze.jsx
+++ src/components/Werkwijze.jsx
@@ -18,7 +18,7 @@ const Werkwijze = () => {
             <div className="p-5 lg:p-10 flex justify-center items-center lg:w-1/2">
               <div className="tracking-widest">
                 <h1 className="py-2 font-bold">Kennismaken</h1>
-                <p className="text-sm">Re-Move Coaching werkt door middel van sessies met verschillende thema’s, elk gericht op het bevorderen van persoonlijke groei en ontwikkeling.</p>
+                <p className="text-sm">Re-Move Coaching werkt door middel van sessies met verschillende thema’s. Tijdens de kennismaking hebben we het over jouw vraag en leren we elkaar kort kennen. Al snel gaan we over op het boksen! Je leert de basistechnieken en we komen samen in beweging. Vooraf kunnen we afspraken maken over de frequentie van de sessies, het tijdstip en het gebruik van de ruimtes. De trainingsruimte kan qua licht en geluid worden aangepast aan jouw voorkeuren! </p>
               </div>
             </div>
           </div>
@@ -31,7 +31,7 @@ const Werkwijze = () => {
             <div className="p-5 lg:p-10 flex justify-center items-center lg:w-1/2">
               <div className="tracking-widest">
                 <h1 className="py-2 font-bold">Boksen</h1>
-                <p className="text-sm">We coachen door middel van boks-technieken die zelfvertrouwen en fysieke kracht bevorderen.</p>
+                <p className="text-sm">We coachen door middel van boks-technieken. In de kennismaking of eerste sessie leren we je de basis van het boksen. In de volgende sessies komen deze basistechnieken terug maar breiden wij die ook uit met alles wat bij boksen komt kijken. Stevige houding, ademhalingstechnieken, concentratie, leiden en volgen, reageren en anticiperen. </p>
               </div>
             </div>
           </div>
@@ -44,7 +44,7 @@ const Werkwijze = () => {
             <div className="p-5 lg:p-10 flex justify-center items-center lg:w-1/2">
               <div className="tracking-widest">
                 <h1 className="py-2 font-bold">Emoties/Gevoelens</h1>
-                <p className="text-sm">Er bestaan veel verschillende emoties en gevoelens waarmee we kunnen werken om veerkracht en emotionele beheersing te ontwikkelen.</p>
+                <p className="text-sm">Er bestaan veel verschillende emoties en gevoelens waar we mee kunnen werken. Ken en herken jij ze allemaal? Vind je ze prettig of soms lastig? En hoe laat jij ze zien? Door sport en spel gaan we samen op ontdekkingstocht. Tijdens de sessies is er ruimte om situaties met elkaar te bespreken en stil te staan bij wat dit met je doet of heeft gedaan. Door dit samen visueel te maken krijg je meer grip op je gevoelens en gedachten en kan je ze beter leren te begrijpen. Wij maken gebruik van verschillende hulpmiddelen zoals: ‘Teken je gesprek’, Helpende gedachten kaartjes, emotie-kaartjes en een emotie plan. </p>
               </div>
             </div>
           </div>
@@ -57,7 +57,7 @@ const Werkwijze = () => {
             <div className="p-5 lg:p-10 flex justify-center items-center lg:w-1/2">
               <div className="tracking-widest">
                 <h1 className="py-2 font-bold">Grenzen (her)kennen</h1>
-                <p className="text-sm">Iedereen kent de zin: STOP, HOU OP! Deze module richt zich op het herkennen en respecteren van persoonlijke grenzen.</p>
+                <p className="text-sm">Iedereen kent de zin: STOP, HOU OP! Maar gebruikt iedereen die zin wel? En is het echt een goede manier om je grenzen aan te geven? Wij helpen je een manier te vinden om je grenzen duidelijk te maken. Maar eerst gaan we ontdekken waar jouw grenzen liggen. Dit doen we zowel in gesprek als tijdens het boksen. </p>
               </div>
             </div>
           </div>
@@ -70,7 +70,7 @@ const Werkwijze = () => {
             <div className="p-5 lg:p-10 flex justify-center items-center lg:w-1/2">
               <div className="tracking-widest">
                 <h1 className="py-2 font-bold">Gedrag</h1>
-                <p className="text-sm">Nu je je emoties en gevoelens kent en je grenzen weet, werken we aan gedragsontwikkeling en zelfdiscipline.</p>
+                <p className="text-sm">Nu je je emoties en gevoelens kent, je grenzen weet en hoe je ze kunt aangeven, gaan we oefenen om jezelf nog beter te begrijpen. Hoe merken anderen dat je boos, verdrietig of juist heel blij bent? Of hoe kan je je gedrag aanpassen zodat een situatie niet meer vervelend verloopt? Daarnaast is herhaling erg belangrijk tijdens onze sessies en zul je hierdoor nieuwe gedragingen eigen gaan maken. Wij willen dat je trots mag en kan zijn op jezelf en sluiten dus elke sessie af met eind oefening waarbij het gevoel van trots centraal staat. </p>
               </div>
             </div>
           </div>
@@ -83,7 +83,7 @@ const Werkwijze = () => {
             <div className="p-5 lg:p-10 flex justify-center items-center lg:w-1/2">
               <div className="tracking-widest">
                 <h1 className="py-2 font-bold">Evaluatie / Einde?</h1>
-                <p className="text-sm">Je bent er bijna! We gaan alles wat je hebt geleerd of nog wilt leren samen doornemen en evalueren.</p>
+                <p className="text-sm">Je bent er bijna! We gaan alles wat je hebt geleerd of nog wilt leren samen doornemen. Ook maken we een Emotie plan dat je thuis, op school of bij je sportclub kunt gebruiken. Dit plan bespreken we met je ouders/verzorgers, en kijken we samen terug op de sessies en wat je geleerd hebt! </p>
               </div>
             </div>
           </div>
